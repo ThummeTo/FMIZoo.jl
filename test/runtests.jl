a87ece109d68b5c60be1caa2577220578a65cc07
TT <tobias.thummerer@informatik.uni-augsburg.de>
2022-03-16 14:07:56 +0100

    fixed test

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,5 +8,14 @@ using FMIZoo
 
 @testset "FMIZoo.jl" begin
     list_models()
-    @test get_model_filename("SpringDamperPendulum1D", "Dymola", "2022x") == "C:\\Users\\thummeto\\Documents\\FMIZoo.jl\\models\\bin\\Dymola\\2022x\\SpringDamperPendulum1D.fmu"
+    
+    path = get_model_filename("SpringDamperPendulum1D", "Dymola", "2022x") 
+    @test length(path) > 0 
+    split = splitpath(path) 
+    @test split[end] ==  "SpringDamperPendulum1D.fmu"
+    @test split[end-1] ==  "2022x"
+    @test split[end-2] ==  "Dymola"
+    @test split[end-3] ==  "bin"
+    @test split[end-4] ==  "models"
+    @test split[end-5] ==  "FMIZoo.jl"
 end
