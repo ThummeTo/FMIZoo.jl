695abd265085e5d7c5059e931c9e58f44fe0e901
TT <tobias.thummerer@informatik.uni-augsburg.de>
2022-04-22 11:58:47 +0200

    added modelica repo FMU backend

--- src/util.jl
+++ src/util.jl
@@ -3,6 +3,9 @@
 # Licensed under the MIT license. See LICENSE file in the project root for details.
 #
 
+import Downloads
+using ZipFile
+
 """
     list_models()
 
@@ -20,7 +23,13 @@ end
 
 Get the filename of a model. `modelName` can be a name or ID. Use `list_models()` to show your options. 
 """
-function get_model_filename(modelName::AbstractString, tool::AbstractString, version::AbstractString)
+function get_model_filename(modelName::AbstractString, tool::AbstractString, version::AbstractString, fmiversion::AbstractString="2.0")
+
+    # workaround to use reference FMUs from the Modelica-Repository
+    if tool == "ModelicaReferenceFMUs"
+        return download_reference_FMU(modelName, version, fmiversion)
+    end
+
     p_tool = joinpath(p_model_bin, tool)
 
     if !isdir(p_tool)
@@ -41,13 +50,50 @@ function get_model_filename(modelName::AbstractString, tool::AbstractString, ver
         error("\"$(version)\" does not specify an existing version. Pick one of these: $(versions)")
     end
 
-    p_model = joinpath(p_ver, modelName * ".fmu")
+    p_fmiver = joinpath(p_ver, fmiversion)
+
+    if !isdir(p_fmiver)
+        g = glob("*", p_ver)
+        fmiversions = map(g) do x
+            splitpath(x)[end]
+        end
+        error("\"$(fmiversion)\" does not specify an existing version. Pick one of these: $(fmiversions)")
+    end
+
+    p_model = joinpath(p_fmiver, modelName * ".fmu")
     if !isfile(p_model)
         println(p_model)
         error("\"$(modelName)\" does not specify an existing model. Do `list_models()` to list your options.")
     end
 
     return p_model
+end
+
+
+function download_reference_FMU(modelName::AbstractString, version::AbstractString="0.0.14", fmiversion::AbstractString="2.0")
+
+    zipPath = Downloads.download("https://github.com/modelica/Reference-FMUs/releases/download/v$(version)/Reference-FMUs-$(version).zip")
+    dir = dirname(zipPath)
+    zarchive = ZipFile.Reader(zipPath)
+
+    path = joinpath(dir, "$(modelName)/")
+    pathToFmu = joinpath(path, "$(modelName).fmu")
+
+    for f in zarchive.files
+        if f.name == "$(fmiversion)/$(modelName).fmu"
+            if !ispath(path)
+                mkdir(path)
+            end
+            
+            numBytes = write(pathToFmu, read(f))
+            if numBytes == 0
+                print("Not able to read!")
+            end
+        end
+    end
+    close(zarchive)
+
+    return pathToFmu
 end
 
 
