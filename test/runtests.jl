6cdfc1e5afe1b9e10b28a681a9989d6c4079e35b
Simon Exner <0815Creeper@users.noreply.github.com>
2024-12-24 19:14:57 +0100

    dbg-test

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,12 +11,15 @@ using FMI, DifferentialEquations
 
 @testset "FMIZoo.jl" begin
     list_models()
+    println("jojojo")
     
     # get_model_filename#1
     path = get_model_filename("SpringDamperPendulum1D", "Dymola", "2022x") 
+    println("nonono")
     @test length(path) > 0 
     split = splitpath(path) 
 
+    println("with the flow")
     @test split[end] == "SpringDamperPendulum1D.fmu"
     @test split[end-1] == "2.0"
     @test split[end-2] == "2022x"
@@ -25,7 +28,10 @@ using FMI, DifferentialEquations
     @test split[end-5] == "models"
     @test split[end-6] == "FMIZoo.jl"
 
+    println("uhm bro")
     path = get_model_filename("BouncingBall", "ModelicaReferenceFMUs", "0.0.14", "3.0")
+    
+    println("wow")
     @test length(path) > 0 
     split = splitpath(path) 
 
@@ -34,21 +40,30 @@ using FMI, DifferentialEquations
     @test split[end-2] == "0.0.14"
     @test split[end-3] == "ModelicaReferenceFMUs"
 
+    println("lopo")
     # generate_mos_scripts
     generate_mos_scripts(verbose=false)
+    
+    println("zolk")
     @test isfile(joinpath(FMIZoo.p_mos_scripts, "Dymola2022x.mos"))
 
+    println("dolf")
     # check available data in VLDM
     cycles = (:train, :validate, :test, "WLTCC2_Low", "WLTCC2_Complete", "Artemis_Road")
+    
+    println("klot")
     lens = (5838, 14446, 11301, 5838, 14446, 11301)
+    
+    println("moos")
     for i in 1:length(cycles)
+        println(string(i))
         cycle = cycles[i]
         len = lens[i]
 
         data = FMIZoo.VLDM(cycle; experiments=1:2)
         data = FMIZoo.VLDM(cycle; experiments=1)
         data = FMIZoo.VLDM(cycle; experiments=2)
-
+        println("vldm")
         for prop ∈ (:position_t, :position_val, :position_dev, 
             :speed_t, :speed_val, :speed_dev, 
             :consumption_t, :consumption_val, :consumption_dev, 
@@ -57,13 +72,20 @@ using FMI, DifferentialEquations
             @test length(getfield(data, prop)) == len
         end
     end
-
+    
+    println(Sys.free_memory() / 2^20)
     # check RobotRR (only availabloe together with FMI)
     data_train = FMIZoo.RobotRR(:train)
+    
+    println(Sys.free_memory() / 2^20)
     tSave = data_train.t 
     tStart = tSave[1]
     @test length(tSave) == 1861
+    
+    println("nop")
     x0 = FMIZoo.getState(data_train, tStart)
+    
+    println("port")
     @test x0 == zeros(6)
 
 end
